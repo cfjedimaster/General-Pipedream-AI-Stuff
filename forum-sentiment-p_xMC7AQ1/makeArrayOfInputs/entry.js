ed335b92a3c291847f3cd7ced9552dfb54bdf2cf
raymondcamden <raymondcamden@gmail.com>
2023-10-15 15:00:23 +0000

    intial

---
+++ forum-sentiment-p_xMC7AQ1/makeArrayOfInputs/entry.js
@@ -0,0 +1,11 @@
+/*
+Given our RSS result, I translate the kinda gross html-ridden values into an array of strings
+*/
+
+export default defineComponent({
+  async run({ steps, $ }) {
+    return steps.merge_rss_feeds.$return_value.map(r => {
+      return r.description.replace(/<.*?>/g, '').replaceAll('&nbsp;',' ').split('. ').slice(0,5).join('. ').trim();
+    });
+  },
+})
