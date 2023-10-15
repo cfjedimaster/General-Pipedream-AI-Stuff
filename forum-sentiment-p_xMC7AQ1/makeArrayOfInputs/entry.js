b094b45630dc762bd08ffcc14614114191ffd81a
raymondcamden <raymondcamden@gmail.com>
2023-10-15 15:01:30 +0000

    only use 10 items from the feed

--- forum-sentiment-p_xMC7AQ1/makeArrayOfInputs/entry.js
+++ forum-sentiment-p_xMC7AQ1/makeArrayOfInputs/entry.js
@@ -6,6 +6,6 @@ export default defineComponent({
   async run({ steps, $ }) {
     return steps.merge_rss_feeds.$return_value.map(r => {
       return r.description.replace(/<.*?>/g, '').replaceAll('&nbsp;',' ').split('. ').slice(0,5).join('. ').trim();
-    });
+    }).slice(0,10);
   },
 })
