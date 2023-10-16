02c81a891646b4ae72397c8bf8f0b8aeac364536
raymondcamden <raymondcamden@gmail.com>
2023-10-16 12:08:57 +0000

    commit before merging to production

--- forum-sentiment-p_xMC7AQ1/craft_email/entry.js
+++ forum-sentiment-p_xMC7AQ1/craft_email/entry.js
@@ -7,16 +7,21 @@ I also define colors for each.
 export default defineComponent({
   async run({ steps, $ }) {
 
-    let ratings = ["Negative","Somewhat positive","Positive"];
+    let ratings = ["Negative","Somewhat negative","Neutral","Somewhat positive","Positive"];
     let totalScore = steps.makeSentimentCalls.$return_value.reduce((prev,v) => {
       return prev + ratings.indexOf(v);
     },0);
     let avg = totalScore / steps.makeSentimentCalls.$return_value.length;
 
+    /*
+    Not happy with this range... but will leave it for now
+    */
     let generalSentiment = '';
-    if(avg < 1) generalSentiment = 'Negative';
-    else if(avg < 2) generalSentiment = 'Somewhat positive';
-    else if(avg <= 3) generalSentiment = 'Positive';
+    if(avg < 0.6) generalSentiment = 'Negative';
+    else if(avg < 1.6) generalSentiment = 'Somewhat negative';
+    else if(avg < 2.6) generalSentiment = 'Neutral';
+    else if(avg < 3.6) generalSentiment = 'Somewhat Positive';
+    else generalSentiment = 'Positive';
 
     let date = new Intl.DateTimeFormat('en-US').format(new Date());
     let email = `
@@ -39,7 +44,8 @@ The general sentiment of the forum is <strong>${generalSentiment}</strong>.
     `;
 
     steps.makeSentimentCalls.$return_value.forEach((s,i) => {
-      if(s === '') s = 'No sentiment detected';
+      // Removed as I set '' to Neutral, kept this here as a reminder
+      //if(s === '') s = 'No sentiment detected';
       let row = `
 <tr>
   <td><a href="${steps.merge_rss_feeds.$return_value[i].link}">${steps.merge_rss_feeds.$return_value[i].title}</a></td><td>${s}</td>
