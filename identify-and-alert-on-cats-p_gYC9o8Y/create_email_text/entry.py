01b0734a621c8ada7808879be7fad881f11fb8ce
raymondcamden <raymondcamden@gmail.com>
2024-12-21 15:16:49 +0000

    commit before merging to production

--- identify-and-alert-on-cats-p_gYC9o8Y/create_email_text/entry.py
+++ identify-and-alert-on-cats-p_gYC9o8Y/create_email_text/entry.py
@@ -1,9 +1,10 @@
 def handler(pd: "pipedream"):
 
   email = f"""
-Matches were found against your image. Matches were found on these targets:
-
-{pd.steps["check_for_targets"]["$return_value"]["matches"]}
+<p>
+Matches were found against your image. Matches were found on these targets:<br>
+<strong>{', '.join(pd.steps["check_for_targets"]["$return_value"]["matches"])}</strong>
+</p>
   """
     
   return {"email": email}
