d4b90b37a86c310c5a1c2c3bcd8109b1b20b235b
raymondcamden <raymondcamden@gmail.com>
2025-07-23 22:46:52 +0000

    commit before merging to production

--- rss-from-gemini-blog-p_G6C5QeB/Get_Entries/entry.py
+++ rss-from-gemini-blog-p_G6C5QeB/Get_Entries/entry.py
@@ -2,6 +2,13 @@ import os
 import requests 
 def handler(pd: "pipedream"):
 
+  # do we have a good cache?
+  cache = pd.inputs["data_store"]
+  cachedData = cache.get("entries")
+  if cachedData:
+    print("cache used")
+    return cachedData
+
   AGENTQL_API_KEY = os.environ.get("AGENTQL_API_KEY")
   URL = "https://developers.googleblog.com/en/search/?product_categories=Gemini"
   
@@ -26,4 +33,7 @@ def handler(pd: "pipedream"):
   }
 
   res = requests.post("https://api.agentql.com/v1/query-data", json=body, headers=headers)
-  return res.json()["data"]["blogposts"]
+
+  entries = res.json()["data"]["blogposts"]
+  cache.set("entries", entries, ttl=3600)
+  return entries
